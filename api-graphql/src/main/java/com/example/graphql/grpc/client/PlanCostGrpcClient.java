a1487d328130f533c2921ffeac1a3b7ee761281d
AmitSingh <AmitKumar.Singh@Trinet.com>
2022-03-21 20:47:30 -0400

    Added resilience4j for circuitbreaker and retry

--- api-graphql/src/main/java/com/example/graphql/grpc/client/PlanCostGrpcClient.java
+++ api-graphql/src/main/java/com/example/graphql/grpc/client/PlanCostGrpcClient.java
@@ -4,22 +4,34 @@ import com.definition.grpc.cost.PlanCost;
 import com.definition.grpc.cost.PlanCostRequest;
 import com.definition.grpc.cost.PlanCostResponse;
 import com.definition.grpc.cost.PlanCostServiceGrpc;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class PlanCostGrpcClient {
 
-    @GrpcClient("cost-service")
+    private static final String COST_SVC = "costSvc";
+    private static final PlanCost.Builder builder = PlanCost.newBuilder().setEmplCovrgRate(0.0f).setEmplrCovrgRate(0.0f);
+
+    @GrpcClient(COST_SVC)
     private PlanCostServiceGrpc.PlanCostServiceBlockingStub planCostServiceBlockingStub;
 
+    @Retry(name = COST_SVC)
+    @CircuitBreaker(name=COST_SVC, fallbackMethod = "fallback")
     public List<PlanCost> planCosts(List<String> codes) {
         PlanCostResponse response = planCostServiceBlockingStub.getPlanCost(PlanCostRequest
                                                                                 .newBuilder()
                                                                                 .addAllPlanCode(codes)
                                                                                 .build());
         return response.getCostList();
+    }
+
+    private List<PlanCost> fallback(List<String> codes, Exception e) {
+        return codes.stream().map(code -> builder.setPlanCode(code).build()).collect(Collectors.toList());
     }
 }
--- api-graphql/src/main/java/com/example/graphql/grpc/client/PlanDocumentGrpcClient.java
+++ api-graphql/src/main/java/com/example/graphql/grpc/client/PlanDocumentGrpcClient.java
@@ -4,22 +4,36 @@ import com.definition.grpc.document.Document;
 import com.definition.grpc.document.DocumentRequest;
 import com.definition.grpc.document.DocumentResponse;
 import com.definition.grpc.document.DocumentServiceGrpc;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class PlanDocumentGrpcClient {
 
-    @GrpcClient("document-service")
+    private static final String DOCUMENT_SVC = "documentSvc";
+    private static final Document.Builder builder = Document.newBuilder()
+                                                            .setDescription("Default SBC")
+                                                            .setPath("/path/to/default.pdf");
+
+    @GrpcClient(DOCUMENT_SVC)
     private DocumentServiceGrpc.DocumentServiceBlockingStub documentServiceBlockingStub;
 
+    @CircuitBreaker(name=DOCUMENT_SVC, fallbackMethod="fallback")
     public List<Document> planDocuments(List<String> titles) {
         DocumentResponse response = documentServiceBlockingStub.getDocuments(DocumentRequest
                                                                                 .newBuilder()
                                                                                 .addAllTitle(titles)
                                                                                 .build());
         return response.getDocumentList();
+    }
+
+    private List<Document> fallback(List<String> titles, Exception e) {
+        return titles.stream().map(title -> builder.setTitle(title).build()).collect(Collectors.toList());
     }
 }
